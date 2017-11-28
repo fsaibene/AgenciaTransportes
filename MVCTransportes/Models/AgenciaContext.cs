526b17c1f490b9841bafb88f0bcb1369f2174e48
Fernando <fernandomsaibene@hotmail.com>
2017-11-28 10:15:45 -0300

    choferes y context

---
+++ MVCTransportes/Models/AgenciaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCTransportes.Models
+{
+    public class AgenciaContext : DbContext
+    {
+        public AgenciaContext() : base("KeyAgenciaDB")
+        {
+
+        }
+        public DbSet<Chofer> Choferes { get; set; }
+        //public DbSet<Vehiculo> Clientes { get; set; }
+    }
+}
---
+++ MVCTransportes/Models/Chofer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTransportes.Models
+{
+    public class Chofer
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DNI { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public int NroRegistro { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
